17af578cb098ba5a7114e40cd931daa9eb10aecc
JokerV <Quocvietcoi2000@gmail.com>
2019-06-09 14:13:31 +0700

    l16 md5

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,4 +1,5 @@
 var db = require('../db');
+var md5 = require('md5');
 
 module.exports.login = function (req, res) {
     res.render('auth/login');
@@ -19,7 +20,10 @@ module.exports.postLogin = function(req, res) {
         });
         return;
     }
-    if(user.password !== password){
+
+    var hashedPassword = md5(password);
+
+    if(user.password !== hashedPassword){
         res.render('auth/login', {
             errors: [
                 'Wrong password.'
