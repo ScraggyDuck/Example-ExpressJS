0733bf87b02fe008ac1a11fcc1da390e04f010cf
JokerV <Quocvietcoi2000@gmail.com>
2019-06-09 10:37:24 +0700

    L14 Cookie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var userRoute = require('./routes/user.route');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 var port = 3000;
@@ -11,6 +12,7 @@ app.set('views', './views');
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(express.static('public'));
+app.use(cookieParser());
 
 // localhost::3000
 app.get('/', function (req, res) {
