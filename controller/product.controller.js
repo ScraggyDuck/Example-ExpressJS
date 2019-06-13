cc7a19ad964a04a5ff9171644e53c51c3dfe9a24
JokerV <Quocvietcoi2000@gmail.com>
2019-06-14 01:09:40 +0700

    Mongoose

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,19 +1,26 @@
-var db = require('../db');
+// var db = require('../db');
+var Product = require('../models/product.model');
 
 module.exports.index = function(req, res){
-    var page = parseInt(req.query.page) || 1;
-    var perPage = 8;
-    var drop = (page - 1) * perPage;
+    // var page = parseInt(req.query.page) || 1;
+    // var perPage = 8;
+    // var drop = (page - 1) * perPage;
 
-    var carts = db.get('sessions').value().cart;
+    // var carts = db.get('sessions').value().cart;
 
-    if(!carts)
-        carts = {sum: 0}
+    // if(!carts)
+    //     carts = {sum: 0}
 
-    res.render('products/index', {
-        products: db.get('products').drop(drop).take(perPage).value(),
-        current: page,
-        pages: 10,
-        carts: carts
-    });
+    // res.render('products/index', {
+    //     products: db.get('products').drop(drop).take(perPage).value(),
+    //     current: page,
+    //     pages: 10,
+    //     carts: carts
+    // });
+    Product.find()
+        .then(function(products){
+            res.render('products/index', {
+                products: products
+            });
+        });
 };
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var csurf = require('csurf');
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL);
 
 var userRoute = require('./routes/user.route');
 var authRoute = require('./routes/authh.route');
